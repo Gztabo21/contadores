c9b7fc88820894e8215643b24607fb2f3f173586
Gztabo21 <gustavocacharuco@gmail.com>
2018-09-06 16:25:54 -0400

    registra se  modifico

--- src/template/header/navbar.php
+++ src/template/header/navbar.php
@@ -3,7 +3,7 @@
 Echo"
 
 <nav class='navbar navbar-expand-lg navbar-dark bg-dark'>
-    <a class='navbar-brand' href='controls\controlsPrincipal.php?num=0'> <img src='src\img\logo.png' width='50 height='50' alt=''> Contadores Publicos</a>
+    <a class='navbar-brand' href='index.php'> <img src='src\img\logo.png' width='50 height='50' alt=''> Contadores Publicos</a>
     <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarText' aria-controls='navbarText' aria-expanded='false' aria-label='Toggle navigation'>
             <span class='navbar-toggler-icon'>
             </span>
@@ -17,7 +17,7 @@ Echo"
                 <div class='collapse navbar-collapse' id='navbarText'>
                   <ul class='navbar-nav mr-auto'>
                     <li class='nav-item active'>
-                      <a class='nav-link' href='controls\controlsPrincipal.php?num=0'>Inicio <span class='sr-only'>(current)</span></a>
+                      <a class='nav-link' href='index.php'>Inicio <span class='sr-only'>(current)</span></a>
                     </li>
 
                     <li class='nav-item'>
@@ -38,9 +38,7 @@ Echo"
           <a class='dropdown-item' href='#'>Something else here</a>
         </div>
       </li>
-      <li class='nav-item'>
-                      <a class='nav-link' href='Contactanos.html'>Contador Online</a>
-                    </li>
+     
                   </ul>
                                   <form class='form-inline'>
                       <a href='#' class='mr-sm-2 my-12'>Registrate <br></a>
